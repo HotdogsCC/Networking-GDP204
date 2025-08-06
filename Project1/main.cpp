d6acc44e2d6193b10a92022fde8b351ff9b26726
Charlie Campbell <chacha9669@hotmail.com>
2025-08-06 10:54:54 +1000

    add the includes

---
+++ Project1/main.cpp
@@ -0,0 +1,10 @@
+#include <GameNetworkingSockets/steam/steamnetworkingsockets.h>
+#include <GameNetworkingSockets/steam/isteamnetworkingutils.h>
+#ifndef STEAMNETWORKINGSOCKETS_OPENSOURCE
+#include <GameNetworkingSockets/steam/steam_api.h>
+#endif
+
+int main()
+{
+	return 0;
+}
